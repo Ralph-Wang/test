67e879915ef6b29aedd858a4868568143a8aa4bc
Ralph <ralph_ld@aliyun.com>
2013-05-06 22:41:53 +0800

    f Test for git

---
+++ final.py
@@ -0,0 +1,48 @@
+﻿#coding=utf-8
+def sort_file(fname):
+        import gen
+        import read
+        import os
+        Dict = read.readXml(fname)
+        gen.GenerateXml(Dict, fname)
+        print("Done..." % fname)
+        #os.system("pause")
+
+def GetFileList(wd):
+    import os
+#    print(wd)
+    os.chdir(wd)
+#    print(os.getcwd())
+    Dir = os.listdir()
+    new_Dir = []
+    for i in Dir:
+        if i.endswith('.xml'):
+            new_Dir.append(i)
+    return new_Dir
+
+def test():
+    #sort_file("test.xml")
+    print(GetFileList())
+
+def controller():
+    import os
+    cwd = os.getcwd()
+    AllDir = os.walk(cwd)
+    for d in AllDir:
+        #print(d[0])
+        if d[0].endswith('new') or d[0].endswith('__'):
+            continue
+        os.chdir(d[0])
+        Dir = GetFileList(d[0])
+        if 'new' not in os.listdir(d[0]):
+            os.mkdir('new')
+        for i in Dir:
+            print("%s being sorted" % i)
+            sort_file(i)
+if __name__ == '__main__':
+    import os
+    #test()
+    try:
+        fileCounts = controller()
+    finally:
+        os.system('pause')
---
+++ gen.py
@@ -0,0 +1,33 @@
+﻿#coding=utf-8
+def GenerateXml(CEdict, fname):
+    import xml.dom.minidom
+    impl = xml.dom.minidom.getDOMImplementation()
+    dom = impl.createDocument(None, 'All', None)
+    root = dom.documentElement
+    keys = sorted(list(CEdict.keys()), key=lambda t:len(t), reverse=1)
+    for i in keys:
+#        new_Dict[i] = Dict[i]
+        #print(i)
+#print(len(Dict))
+        string = dom.createElement('string')
+        root.appendChild(string)
+        chineseE=dom.createElement('chinese')
+        chineseT=dom.createTextNode(i)
+        chineseE.appendChild(chineseT)
+        string.appendChild(chineseE)
+
+        englishE=dom.createElement('english')
+        englishT=dom.createTextNode(CEdict[i])
+        englishE.appendChild(englishT)
+        string.appendChild(englishE)
+
+
+    f= open('new/%s' % (fname), 'w', encoding='utf-8')
+    dom.writexml(f, addindent='  ', newl='\n',encoding='utf-8')
+    f.close()
+
+def test():
+    GenerateXml({"测试":"test","测试2":"test2"})
+
+if __name__ == '__main__':
+    test()
