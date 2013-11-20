2f7af1ed62562fc8d91ffc3b3674cfd91989f63c
Ralph-Wang <ralph_ld@aliyun.com>
2013-11-20 16:13:56 +0800

    An implementation for calling methods in seccession

---
+++ python/returnSelf.py
@@ -0,0 +1,16 @@
+from distutils import log
+
+class ReturnSelf(object):
+  def __init__(self,theId=1):
+    self._id = theId
+
+  def show(self):
+    log.warn(self._id)
+    return self
+
+if __name__ == '__main__':
+  test = ReturnSelf(10)
+  # show the _id 3 times
+  test.show()\
+      .show()\
+      .show()
