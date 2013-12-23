845d153a81e2a156b7a21cbfb847a69c0bb86322
Ralph-Wang <ralph_ld@aliyun.com>
2013-12-23 23:25:26 +0800

    practice of core programming python

---
+++ python/re/re15-13.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# core python programming: practice 15-13
+# a function to get type name from type(obj)
+
+import re
+
+def get_type(obj):
+  s = str(type(obj))
+  #print s
+  p = re.compile(r"<\w+ '(.+)'>")
+  return p.match(s).group(1)
+
+class test:
+  pass
+
+class test2(object):
+  pass
+
+
+if __name__ == '__main__':
+  print '0 is:', get_type(0)
+  print "'' is:", get_type('')
+  print 'get_type is:', get_type(get_type)
+  print '[] is:', get_type([])
+  print '() is:', get_type(())
+  print '{} is:', get_type({})
+  print 'test is:', get_type(test)
+  print 'test() is:', get_type(test())
+  print 'test2 is:', get_type(test2)
+  print 'test2() is:', get_type(test2())
